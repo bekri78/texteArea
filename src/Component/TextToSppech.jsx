4b85ec7d03979d52bad7f03582b243f311051d72
Snouzy <mathiasnouzy@gmail.com>
2021-04-14 00:36:28 +0200

    feat: debug

--- src/Component/TextToSppech.jsx
+++ src/Component/TextToSppech.jsx
@@ -1,12 +1,13 @@
-import React, { useState } from 'react';
+import React, { useState, useCallback } from 'react';
 import { useSpeechSynthesis } from 'react-speech-kit';
+import { makeStyles } from '@material-ui/core/styles';
+import { AccessAlarm } from '@material-ui/icons';
+import { Box, TextField, Typography, CardContent, Card } from '@material-ui/core';
+
 import Espace from './Espace';
 import Couleur from './Couleur';
 import Voyelles from './Voyelles';
 import Police from './Police';
-import { makeStyles } from '@material-ui/core/styles';
-import { AccessAlarm } from '@material-ui/icons';
-import TextField from '@material-ui/core/TextField';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -19,7 +20,8 @@ const useStyles = makeStyles((theme) => ({
     width: '25ch',
   },
   input: {
-    height: 100,
+    height: 300,
+    margin: 8,
   },
   color: {
     display: 'flex',
@@ -27,40 +29,72 @@ const useStyles = makeStyles((theme) => ({
       margin: theme.spacing(1),
     },
   },
+  containerWrapper: {
+    width: '100%',
+    display: 'grid',
+    gridTemplateColumns: '1fr 1fr',
+    gridColumnGap: '10px',
+  },
 }));
 
 function TextToSpeech() {
   const classes = useStyles();
-  const [value, setValue] = useState('');
+  const [value, setValue] = useState();
+  const [modifiedValue, setModifiedValue] = useState([]);
   const { speak } = useSpeechSynthesis();
+
+  const handleValueChange = useCallback((event) => {
+    setValue(event.target.value);
+  }, []);
+
+  const handleTextModifier = useCallback((newText) => {
+    setModifiedValue(newText);
+  });
 
   return (
     <>
+      <div>{modifiedValue.map((letter) => letter)}</div>
+
       <div className={classes.color}>
         <Espace />
         <Couleur />
-        <Voyelles />
+        <Voyelles textModifier={handleTextModifier} value={value} />
         <Police />
       </div>
+
       <div className={classes.root}>
-        <TextField
-          id="filled-full-width"
-          label="AlphaB"
-          style={{ margin: 8 }}
-          placeholder="Entrer votre texte"
-          helperText="Facilitons la lecture !"
-          fullWidth
-          margin="normal"
-          InputLabelProps={{
-            shrink: true,
-          }}
-          InputProps={{
-            className: classes.input,
-          }}
-          variant="filled"
-          value={value}
-          onChange={(event) => setValue(event.target.value)}
-        />
+        <Box className={classes.containerWrapper}>
+          <TextField
+            id="filled-full-width"
+            label="AlphaB"
+            placeholder="Entrer votre texte..."
+            helperText="Facilitons la lecture !"
+            multiline
+            rows={10}
+            margin="normal"
+            InputLabelProps={{
+              shrink: true,
+            }}
+            variant="filled"
+            value={value}
+            className={classes.input}
+            onChange={handleValueChange}
+          />
+
+          <div style={{ wordBreak: 'break-all' }}>
+            <Card className={classes.root}>
+              <CardContent>
+                <Typography className={classes.title} color="textSecondary" gutterBottom>
+                  Couleur voyelles
+                </Typography>
+                <Typography variant="h5" component="h2">
+                  {modifiedValue.map((letter) => letter)}
+                </Typography>
+              </CardContent>
+            </Card>
+          </div>
+        </Box>
+
         <AccessAlarm onClick={() => speak({ text: value })} />
       </div>
     </>
--- src/Component/Voyelles.jsx
+++ src/Component/Voyelles.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/prop-types */
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import InputLabel from '@material-ui/core/InputLabel';
@@ -25,15 +26,33 @@ const useStyles = makeStyles((theme) => ({
   gris: {
     backgroundColor: '#C0C0C0',
   },
+  jaune: {
+    backgroundColor: '#FFFF00',
+  },
 }));
 
-export default function Voyelles() {
+export default function Voyelles(props) {
   const classes = useStyles();
-  const [age, setAge] = React.useState('');
+  const [colorValue, setColorValue] = React.useState('');
   const [open, setOpen] = React.useState(false);
+  const textValue = props.value;
+  const handleChange = (e) => {
+    setColorValue(e.target.value);
 
-  const handleChange = (event) => {
-    setAge(event.target.value);
+    let newArea = [];
+
+    if (textValue) {
+      const valueEntered = textValue.split('');
+      for (let i = 0; i < valueEntered.length; i++) {
+        if (['a', 'e', 'i', 'o', 'u'].includes(valueEntered[i])) {
+          newArea.push(<span style={{ color: e.target.value }}>{valueEntered[i]}</span>);
+        } else {
+          newArea.push(valueEntered[i]);
+        }
+      }
+    }
+
+    return props.textModifier(newArea);
   };
 
   const handleClose = () => {
@@ -44,33 +63,54 @@ export default function Voyelles() {
     setOpen(true);
   };
 
+  const config = [
+    {
+      id: 1,
+      value: 'gray',
+      class: 'gris',
+      letter: 'G',
+    },
+    {
+      id: 2,
+      value: 'red',
+      class: 'rouge',
+      letter: 'R',
+    },
+    {
+      id: 3,
+      value: 'green',
+      class: 'vert',
+      letter: 'V',
+    },
+    {
+      id: 4,
+      value: 'yellow',
+      class: 'jaune',
+      letter: 'J',
+    },
+  ];
+
   return (
     <div>
       <Button className={classes.button} onClick={handleOpen}>
         Couleur Voyelles
       </Button>
+
       <FormControl className={classes.formControl}>
-        <InputLabel id="demo-controlled-open-select-label">Selectionner </InputLabel>
+        <InputLabel id="demo-controlled-open-select-label">Selectionner</InputLabel>
         <Select
           labelId="demo-controlled-open-select-label"
           id="demo-controlled-open-select"
           open={open}
           onClose={handleClose}
           onOpen={handleOpen}
-          value={age}
+          value={colorValue}
           onChange={handleChange}>
-          <MenuItem value={'orange'}>
-            {' '}
-            <Avatar className={classes.gris}>Gi</Avatar>
-          </MenuItem>
-          <MenuItem value={'rouge'}>
-            {' '}
-            <Avatar className={classes.rouge}>Ro</Avatar>
-          </MenuItem>
-          <MenuItem value={'vert'}>
-            {' '}
-            <Avatar className={classes.vert}>Ve</Avatar>
-          </MenuItem>
+          {config.map((item) => (
+            <MenuItem value={item.value} key={item.id}>
+              <Avatar className={classes[item.class]}>{item.letter}</Avatar>
+            </MenuItem>
+          ))}
         </Select>
       </FormControl>
     </div>
