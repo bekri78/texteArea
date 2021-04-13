78bc2f9efa9d96a06d691a874e4650f5e99ba677
bekri78 <bekri.mehdi@outlook.com>
2021-04-13 02:09:47 +0200

    react-text-to-speech and material ui text area

---
+++ src/Component/Couleur.jsx
@@ -0,0 +1,81 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
+import Button from '@material-ui/core/Button';
+import Avatar from '@material-ui/core/Avatar';
+import { deepOrange, deepPurple } from '@material-ui/core/colors';
+
+const useStyles = makeStyles((theme) => ({
+  button: {
+    display: 'block',
+    marginTop: theme.spacing(2),
+  },
+  formControl: {
+    margin: theme.spacing(1),
+    minWidth: 120,
+  },
+  orange: {
+    color: theme.palette.getContrastText(deepOrange[500]),
+    backgroundColor: deepOrange[500],
+  },
+  blue: {
+    backgroundColor: '#f1d40e',
+  },
+  purple: {
+    color: theme.palette.getContrastText(deepPurple[500]),
+    backgroundColor: deepPurple[500],
+  },
+}));
+
+export default function Couleur() {
+  const classes = useStyles();
+  const [age, setAge] = React.useState('');
+  const [open, setOpen] = React.useState(false);
+
+  const handleChange = (event) => {
+    setAge(event.target.value);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  return (
+    <div>
+      <Button className={classes.button} onClick={handleOpen}>
+        Couleur Texte
+      </Button>
+      <FormControl className={classes.formControl}>
+        <InputLabel id="demo-controlled-open-select-label">Selectionner </InputLabel>
+        <Select
+          labelId="demo-controlled-open-select-label"
+          id="demo-controlled-open-select"
+          open={open}
+          onClose={handleClose}
+          onOpen={handleOpen}
+          value={age}
+          onChange={handleChange}>
+          <MenuItem value={'orange'}>
+            {' '}
+            <Avatar className={classes.orange}>Or</Avatar>
+          </MenuItem>
+          <MenuItem value={'jaune'}>
+            {' '}
+            <Avatar className={classes.blue}>Ja</Avatar>
+          </MenuItem>
+          <MenuItem value={'purple'}>
+            {' '}
+            <Avatar className={classes.purple}>Vi</Avatar>
+          </MenuItem>
+        </Select>
+      </FormControl>
+    </div>
+  );
+}
