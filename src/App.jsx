c8b92f14b8904977a1c2a58fa3db85b23ec9ce0e
scothy <sokthy77@hotmail.com>
2021-04-21 15:35:24 +0200

    add nav router

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,11 @@
 import React from 'react';
-import TextToSpeech from './views/TextToSpeech';
-import SimpleAccordion from './components/Questions/questions.jsx';
 import HideAppBar from './components/Navbar/navbar';
+
 import './App.css';
 function App() {
   return (
     <div>
       <HideAppBar />
-      <SimpleAccordion />
-      <TextToSpeech />
     </div>
   );
 }
