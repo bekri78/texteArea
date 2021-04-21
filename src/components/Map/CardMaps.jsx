b3305c9af7cd47af81a57577ab44ebba2f709934
Emeline <leduc.emeline44@gmail.com>
2021-04-21 16:45:22 +0200

    add map and current location

--- src/components/Map/CardMaps.jsx
+++ src/components/Map/CardMaps.jsx
@@ -1,9 +1,54 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import GoogleMapReact from 'google-map-react';
+import Marker from './Marker/Marker.tsx';
+import './CardMaps.css';
 
 function CardMaps() {
+  const [lat, setLat] = useState(null);
+  const [lng, setLng] = useState(null);
+  const [error, setError] = useState('');
+  const center = { lat: lat, lng: lng };
+
+  useEffect(() => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(location, errorLocation);
+    }
+  }, [lat, lng]);
+
+  const location = (position) => {
+    setLat(position.coords.latitude);
+    setLng(position.coords.longitude);
+  };
+
+  const errorLocation = (error) => {
+    switch (error.code) {
+      case error.PERMISSION_DENIED:
+        return setError('Géolocalisation refusée');
+      case error.POSITION_UNAVAILABLE:
+        return setError('Position inconnue');
+      case error.TIMEOUT:
+        return setError('Chargement de la positon expiré');
+      case error.UNKNOWN_ERROR:
+        return setError('Erreur inconnue');
+      default:
+        return setError('Erreur inconnue');
+    }
+  };
+
   return (
     <div>
       <h1>Map</h1>
+      <div id="map">
+        <GoogleMapReact
+          bootstrapURLKeys={{
+            key: 'AIzaSyAURsom7c-jmbNERN0wVqb4OzVten2Hy24',
+          }}
+          center={center}
+          zoom={12}>
+          <Marker lat={lat} lng={lng} color="red" text="my-marker" />
+        </GoogleMapReact>
+        {error && <h1>{error}</h1>}
+      </div>
     </div>
   );
 }
